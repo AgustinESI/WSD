6e17ea6f99dc89e18232afd43d174826d49e6140
Agustin Sanchez <Agustin.Sanchez9@alu.uclm.es>
2025-02-06 08:57:06 +0100

    first commit

---
+++ src/main/java/com/uclm/esi/l02/WSD/WsdApplication.java
@@ -0,0 +1,13 @@
+package com.uclm.esi.l02.WSD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uclm/esi/l02/WSD/WsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uclm.esi.l02.WSD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
